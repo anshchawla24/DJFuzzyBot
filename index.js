35fc7b37da4696f6c3e82f5567a6b5f33b20ee28
anshchawla24 <anshchawla2201@gmail.com>
2020-05-21 19:09:47 +0530

    added new port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const token = process.env.TOKEN;
 const PREFIX = '$';
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.get('/',(req, res) => {
     res.send("Fuzzy says hello!");
 });
